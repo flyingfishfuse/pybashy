998701c15e3b165ae68714d978bc73030746189c
flyingfishfusealt <46322912+flyingfishfuse@users.noreply.github.com>
2020-08-28 15:22:37 -0500

    Update loader.py

--- pybashy/loader.py
+++ pybashy/loader.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ################################################################################
-##                         pybashy - commandrunner.py                         ##
+##                             pybashy - loader.py                            ##
 ################################################################################
 # Copyright (c) 2020 Adam Galindo                                             ##
 #                                                                             ##
@@ -26,10 +26,10 @@
 # THE SOFTWARE.                                                               ##
 ################################################################################
 """
-Infrastructure that allows for running bash scripts with python
+This file contains the argument and configuration parsing code
 """
-__author__ 	= 'Adam Galindo'
-__email__ 	= 'null@null.com'
+__author__     = 'Adam Galindo'
+__email__     = 'null@null.com'
 __version__ = '1'
 __license__ = 'GPLv3'
 
@@ -47,6 +47,7 @@ import threading
 import configparser
 
 #import the framework
+import pybashy.pybashy
 from pybashy.commandrunner import Command
 from pybashy.commandrunner import CommandSet
 from pybashy.commandrunner import CommandRunner
@@ -54,121 +55,90 @@ from pybashy.commandrunner import ExecutionPool
 from pybashy.useful_functions import error_message
 from pybashy.useful_functions import greenprint,yellow_bold_print,redprint
 from pybashy.useful_functions import info_message,warning_message,critical_message
-#####################################################################################################################################################################
-# Commandline Arguments
-###################################################################################
-# If the user is running the program as a script we parse the arguments or use the 
-# config file. 
-# If the user is importing this as a module for usage as a command framework we do
-# not activate the argument or configuration file parsing engines
-parser = argparse.ArgumentParser(description='python based, bash task execution manager')
-parser.add_argument('--testing',
-							 dest		= 'testing',
-							 action		= "store_true" ,
-							 help		= 'will run a series of tests, testing modules not supported yet' )
-parser.add_argument('--use-config',
-							 dest		= 'config_file',
-							 action		= "store_true" ,
-							 help		= 'Use config file, if used, will ignore other options' )
-parser.add_argument('--config-filename',
-							 dest		= 'config_filename',
-							 action		= "store" ,
-							 help		= 'Name of the config file' )
-parser.add_argument('--execute-module',
-							 dest		= 'dynamic_import',
-							 action		= "store_true" ,
-							 help		= 'Will execute user created module if used, will ignore config options ' )
-parser.add_argument('--module-name',
-							 dest		= 'dynamic_import_name',
-							 action		= "store" ,
-							 help		= 'Name of module to load' )
 
 
 if __name__ == "__main__":
 
-	def list_modules(self):
-		'''
-	Lists modules in command_set directory
-		'''
-		list_of_modules = []
-		command_files_dir = os.path.dirname(__file__) + "/commandset"		
-		list_of_subfiles  = pkgutil.iter_modules([command_files_dir])
-		for filez in list_of_subfiles:
-			print(filez.name)
-			list_of_modules.append(filez.name)
-		return list_of_modules
+    def list_modules():
+        '''
+    Lists modules in command_set directory
+        '''
+        list_of_modules = []
+        command_files_dir = os.path.dirname(__file__) + "/commandset"        
+        list_of_subfiles  = pkgutil.iter_modules([command_files_dir])
+        for filez in list_of_subfiles:
+            print(filez.name)
+            list_of_modules.append(filez.name)
+        return list_of_modules
 
-	def execute_test():
-			#asdf = Stepper()
-			#asdf.step_test(asdf.example)
-			#asdf.step_test(asdf.example2)
-			# move to pybashy.py maybe?######################################
-			global execution_pool                                           
-			execution_pool = ExecutionPool()
-			command_runner = CommandRunner()
-			command_pool   = command_runner.dynamic_import('commandtest')
-			for command_name , command_set in command_pool: 
-				execution_pool.command_set_dict[command_name] = command_set #
-			#################################################################
-			# printing the contents
-			command_set_class = execution_pool.command_set_dict.items[0]
-			task = execution_pool.worker_bee(command_set_class)
-			for name, thing in dir(execution_pool.command_set_dict.items):
-				yellow_bold_print(name)
-				yellow_bold_print(thing)
-			
-			for command_name,command_set_object in execution_pool.command_set_dict.items():
-				for thing_name in dir(command_set_object):
-					if thing_name.startswith('__') != True:
-						yellow_bold_print(thing_name)
-			#finished_task = new_stepper.worker_bee(new_command_set_class,)
+    def execute_test():
+            #asdf = Stepper()
+            #asdf.step_test(asdf.example)
+            #asdf.step_test(asdf.example2)
+            # move to pybashy.py maybe?######################################
+            global execution_pool                                           
+            execution_pool = ExecutionPool()
+            command_runner = CommandRunner()
+            command_pool   = command_runner.dynamic_import('commandtest')
+            for command_name , command_set in command_pool: 
+                execution_pool.command_set_dict[command_name] = command_set #
+            #################################################################
+            # printing the contents
+            command_set_class = execution_pool.command_set_dict.items()[0]
+            execution_pool.worker_bee(command_set_class)
+            for name, thing in dir(execution_pool.command_set_dict.items):
+                yellow_bold_print(name)
+                yellow_bold_print(thing)
+            
+            for command_name,command_set_object in execution_pool.command_set_dict.items():
+                for thing_name in dir(command_set_object):
+                    if thing_name.startswith('__') != True:
+                        yellow_bold_print(thing_name)
+            #finished_task = new_stepper.worker_bee(new_command_set_class,)
 
 # is this needed?
-	def load_modules_from_config():
-		module_pool = {}
-		module_loader = CommandRunner()
-		if extension in module_loader.list_modules():
-			module_pool[extension] = module_loader.dynamic_import(extension)
-		else:
-			yellow_bold_print("[-] Module not in framework : " + str(extension))
-			raise SystemExit
-			sys.exit()
-		# now that the modules are loaded, assign options to kwargs
-		kwargs 	= {}
-		for option, value in config[user_choice]:
-			kwargs[option] = value
-			thing_to_do = CommandSet(kwargs)
-		
-
-
-	arguments = parser.parse_args()
-	if arguments.testing == True and (arguments.config_file != True):
-		execute_test()
-	#are we using config?
-	if arguments.config_file == True and (arguments.testing != True):
-		config = configparser.ConfigParser()
-		config.read(arguments.config_path)
-		# user needs to set config file or arguments
-		user_choice = config['Thing To Do']['choice']
-		if user_choice== 'doofus':
-			yellow_bold_print("YOU HAVE TO CONFIGURE THE DARN THING FIRST!")
-			raise SystemExit
-			sys.exit()
-		# Doesnt run for choice = DEFAULT unless (look down)
-		elif user_choice == 'DEFAULT':
-			execute_test()
-		#if the user chose a section in the config file
-		elif user_choice in config.sections:
-			#find the list of modules they want to load
-			modules_to_load:list = config['Thing To Do']['modules'].split(',')
-			#sort through them to see if they are requesting something that actually exists
-			for extension in modules_to_load:
-				# make a pool of CommandSet() objects in a dict, named after the module
-				load_modules_from_config()
-		else:
-			redprint("[-] Option not in config file")
- 	#loading a specialized module
-	elif arguments.config_file == False and (arguments.dynamic_import == True):
-		new_command = CommandRunner()
-		new_command_pool = new_command.dynamic_import(arguments.dynamic_import_name)
+    def load_modules_from_config():
+        module_pool = {}
+        module_loader = CommandRunner()
+        if extension in list_modules():
+            module_pool[extension] = module_loader.dynamic_import(extension)
+        else:
+            yellow_bold_print("[-] Module not in framework : " + str(extension))
+            raise SystemExit
+            sys.exit()
+        # now that the modules are loaded, assign options to kwargs
+        kwargs     = {}
+        for option, value in config[user_choice]:
+            kwargs[option] = value
+            thing_to_do = CommandSet(kwargs)
+        
+    if arguments.testing == True and (arguments.config_file != True):
+        execute_test()
+    #are we using config?
+    if arguments.config_file == True and (arguments.testing != True):
+        config = configparser.ConfigParser()
+        config.read(arguments.config_path)
+        # user needs to set config file or arguments
+        user_choice = config['Thing To Do']['choice']
+        if user_choice== 'doofus':
+            yellow_bold_print("YOU HAVE TO CONFIGURE THE DARN THING FIRST!")
+            raise SystemExit
+            sys.exit()
+        # Doesnt run for choice = DEFAULT unless (look down)
+        elif user_choice == 'DEFAULT':
+            execute_test()
+        #if the user chose a section in the config file
+        elif user_choice in config.sections:
+            #find the list of modules they want to load
+            modules_to_load:list = config['Thing To Do']['modules'].split(',')
+            #sort through them to see if they are requesting something that actually exists
+            for extension in modules_to_load:
+                # make a pool of CommandSet() objects in a dict, named after the module
+                load_modules_from_config()
+        else:
+            redprint("[-] Option not in config file")
+     #loading a specialized module
+    elif arguments.config_file == False and (arguments.dynamic_import == True):
+        new_command = CommandRunner()
+        new_command_pool = new_command.dynamic_import(arguments.dynamic_import_name)
 
