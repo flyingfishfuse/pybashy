fa472ef519f4e138c901e1cc5144140830e1aed2
flyingfishfusealt <46322912+flyingfishfuse@users.noreply.github.com>
2020-08-28 05:20:28 -0500

    Update loader.py

--- pybashy/loader.py
+++ pybashy/loader.py
@@ -1,37 +1,32 @@
 # -*- coding: utf-8 -*-
 ################################################################################
-##			debootstrapy - a linux tool for using debootstrap				  ##
+##                         pybashy - commandrunner.py                         ##
 ################################################################################
-# Copyright (c) 2020 Adam Galindo											  ##
-#																			  ##
+# Copyright (c) 2020 Adam Galindo                                             ##
+#                                                                             ##
 # Permission is hereby granted, free of charge, to any person obtaining a copy##
 # of this software and associated documentation files (the "Software"),to deal##
 # in the Software without restriction, including without limitation the rights##
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell   ##
-# copies of the Software, and to permit persons to whom the Software is		  ##
-# furnished to do so, subject to the following conditions:					  ##
-#																			  ##
-# Licenced under GPLv3														  ##
-# https://www.gnu.org/licenses/gpl-3.0.en.html								  ##
-#																			  ##
+# copies of the Software, and to permit persons to whom the Software is       ##
+# furnished to do so, subject to the following conditions:                    ##
+#                                                                             ##
+# Licenced under GPLv3                                                        ##
+# https://www.gnu.org/licenses/gpl-3.0.en.html                                ##
+#                                                                             ##
 # The above copyright notice and this permission notice shall be included in  ##
-# all copies or substantial portions of the Software.						  ##
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-####
+# all copies or substantial portions of the Software.                         ##
+#                                                                             ##
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  ##
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,    ##
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE ##
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER      ##
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,#
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN   ##
+# THE SOFTWARE.                                                               ##
 ################################################################################
 """
-TODO: build up the execution pool class ya daft loon!
-its in commandrunner.py now
-	- infrastructure that allows for running bash scripts with python
-
-
+Infrastructure that allows for running bash scripts with python
 """
 __author__ 	= 'Adam Galindo'
 __email__ 	= 'null@null.com'
@@ -51,6 +46,7 @@ import threading
 import configparser
 
 #import the framework
+from pybashy.commandrunner import Command
 from pybashy.commandrunner import CommandSet
 from pybashy.commandrunner import CommandRunner
 from pybashy.commandrunner import ExecutionPool
@@ -89,21 +85,43 @@ parser.add_argument('--module-name',
 
 if __name__ == "__main__":
 
+	def list_modules(self):
+		'''
+	Lists modules in command_set directory
+		'''
+		list_of_modules = []
+		command_files_dir = os.path.dirname(__file__) + "/commandset"		
+		list_of_subfiles  = pkgutil.iter_modules([command_files_dir])
+		for filez in list_of_subfiles:
+			print(filez.name)
+			list_of_modules.append(filez.name)
+		return list_of_modules
+
 	def execute_test():
 			#asdf = Stepper()
 			#asdf.step_test(asdf.example)
 			#asdf.step_test(asdf.example2)
-			exec_pool   = ExecutionPool()
-			exec_pool(CommandRunner().dynamic_import('commandtest'))
-			for command_name,command_set_object in exec_pool.items():
+			# move to pybashy.py maybe?######################################
+			global execution_pool                                           
+			execution_pool = ExecutionPool()
+			command_runner = CommandRunner()
+			command_pool   = command_runner.dynamic_import('commandtest')
+			for command_name , command_set in command_pool: 
+				execution_pool.command_set_dict[command_name] = command_set #
+			#################################################################
+			# printing the contents
+			for name, thing in dir(execution_pool.command_set_dict.items):
+				yellow_bold_print(name)
+				yellow_bold_print(thing)
+			
+			for command_name,command_set_object in execution_pool.command_set_dict.items():
 				for thing_name in dir(command_set_object):
-					print(command_name)
 					if thing_name.startswith('__') != True:
 						yellow_bold_print(thing_name)
-			
 			#finished_task = new_stepper.worker_bee(new_command_set_class,)
 
-	def load_modules():
+# is this needed?
+	def load_modules_from_config():
 		module_pool = {}
 		module_loader = CommandRunner()
 		if extension in module_loader.list_modules():
@@ -112,13 +130,12 @@ if __name__ == "__main__":
 			yellow_bold_print("[-] Module not in framework : " + str(extension))
 			raise SystemExit
 			sys.exit()
-					# now that the modules are loaded, assign options to kwargs
+		# now that the modules are loaded, assign options to kwargs
 		kwargs 	= {}
 		for option, value in config[user_choice]:
 			kwargs[option] = value
+			thing_to_do = CommandSet(kwargs)
 		
-		#thing_to_do = CommandSet(kwargs)
-		#modu
 
 
 	arguments = parser.parse_args()
@@ -144,7 +161,7 @@ if __name__ == "__main__":
 			#sort through them to see if they are requesting something that actually exists
 			for extension in modules_to_load:
 				# make a pool of CommandSet() objects in a dict, named after the module
-				load_modules()
+				load_modules_from_config()
 		else:
 			redprint("[-] Option not in config file")
  	#loading a specialized module
